deca0e8043d4080b71e57f9474f4808c23ee7f4f
Gabs Momilli <gabsmomilli@gmail.com>
2020-01-10 23:14:55 -0200

    notes

---
+++ notes.py
@@ -0,0 +1,14 @@
+n1 = input('insira a primeira nota: ')
+n2 = input('insira a segunda nota: ')
+n3 = input('insira a terceira nota: ')
+n4 = input('insira a quarta nota: ')
+n5 = input('insira a quinta nota: ')
+
+p1 = 1
+p2 = 2
+p3 = 3
+p4 = 4
+p5 = 5
+
+resultado = ((n1 * p1) + (n2 * p2) + (n3 * p3) + (n4 * p4) + (n5 * p5)) / (p1 + p2 + p3 + p4 + p5)
+print (resultado)
