f3f65fdb024d31c9597e7d1b8a848fecae5bfc33
Gabs Momilli <gabsmomilli@gmail.com>
2020-01-21 21:14:49 -0200

    divisao

---
+++ divisao.py
@@ -0,0 +1,7 @@
+x = int(input("Escolha um numero para ser o dividendo: "))
+y = int(input("Escolha um numero para ser o divisor: "))
+
+if y == 0:
+	print ("Valor invalido. Digite outro numero \n")
+
+print (x / y)
