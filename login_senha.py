fcb49adb8eb8fa2ed3bb04a32947dc951745c68c
Gabs Momilli <gabsmomilli@gmail.com>
2020-01-23 21:16:16 -0200

    login e senha

---
+++ login_senha.py
@@ -0,0 +1,7 @@
+login = input("login: ")
+senha = input("senha: ")
+
+while login == senha:
+    print("login e senha identicos\nINFORME NOVAMENTE")
+    login = input("login: ")
+    senha = input("senha: ")
