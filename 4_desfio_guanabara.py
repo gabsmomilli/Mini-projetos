da9078254bcc5a4ac18b082dad8a4465b8986520
Gabs Momilli <gabsmomilli@gmail.com>
2020-01-29 19:45:34 -0200

    quarto desafio - Guanabara

---
+++ 4_desfio_guanabara.py
@@ -0,0 +1,3 @@
+n = input("digite algo: ")
+print("o que vc escreveu eh numero:", n.isnumeric(), "\no que vc escrveu eh string:", n.isalpha(),
+      "\no que vc escreveu eh minusculo:", n.islower(), "\no que vc escreveu eh maiuscula:", n.isupper())
