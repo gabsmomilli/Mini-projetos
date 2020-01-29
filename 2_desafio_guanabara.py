c92a7aff5d2fd5ab942d88007aa1c857ba635aec
Gabs Momilli <gabsmomilli@gmail.com>
2020-01-29 19:20:57 -0200

    segundo desafio - Guanabara

---
+++ 2_desafio_guanabara.py
@@ -0,0 +1,4 @@
+dia = input("informe o dia do seu nascimento: ")
+mes = input("informe o mes do seu nascimento: ")
+ano = input("informe o ano do seu nascimento: ")
+print(dia + "/" + mes + "/" + ano)
