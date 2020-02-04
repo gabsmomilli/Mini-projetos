dbba6e6fb82091603beb45713237cbfe3a78840e
Gabs Momilli <gabsmomilli@gmail.com>
2020-02-03 23:51:57 -0200

    decimo desafio - Guanabara

---
+++ Curso em video/10_desafio_guanabara.py
@@ -0,0 +1,4 @@
+reais = float(input("quantos reais voce tem na carteira? "))
+dolar = float(input("quanto esta valendo o dolar em reais? "))
+dr= reais // dolar
+print("voce pode comprar {} em dolares".format(dr))
