30d5c1af8911e0a011bed3431a48f4754097550b
Gabs Momilli <gabsmomilli@gmail.com>
2020-02-04 00:07:21 -0200

    decimo primeiro desafio - Guanabara

---
+++ Curso em video/11_desafio_guanabara.py
@@ -0,0 +1,9 @@
+altura = float(input("informe a altura da parede: "))
+largura = float(input("informe a largura da parede: "))
+area = altura * largura
+tinta =float(1*(2**2))
+tinta = float (area/tinta)
+print("-"*50)
+print("sua parede tem {} metros quadrados".format(area))
+print("voce precisa de {} litros para pintar esta parede".format(tinta))
+print("-"*50)
